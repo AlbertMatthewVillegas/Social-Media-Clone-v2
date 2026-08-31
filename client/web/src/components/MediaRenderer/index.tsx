60f4436c267a11d2943eca8752934c24a03e18ca
Albert Matthew Villegas <12mnsin@gmail.com>
2026-08-31 20:00:04 +0800

    refactor: simplify MediaRendererProps by removing idx and adjusting media rendering logic

--- client/web/src/components/MediaRenderer/index.tsx
+++ client/web/src/components/MediaRenderer/index.tsx
@@ -1,21 +1,20 @@
 interface MediaRendererProps {
-    src: string, 
-    idx: number
+    src: string | undefined, 
 }
 
-function MediaRenderer({ src, idx }:MediaRendererProps) {
+function MediaRenderer({ src }: MediaRendererProps) {
     if (!src) return null;
     const s = src.toLowerCase();
     if (s.startsWith("data:video/") || s.match(/\.(mp4|webm|ogg)(\?.*)?$/)) {
         return (
-            <video key={idx} className="flex w-full h-full rounded" controls>
+            <video className="flex w-full h-full rounded object-contain" controls>
                 <source src={src} />
                 Your browser does not support the video tag.
             </video>
         );
     }
     if (s.startsWith("data:image/") || s.match(/\.(png|jpe?g|gif|svg)(\?.*)?$/)) {
-        return <img key={idx} className="flex w-full h-full rounded" src={src} alt={`post-${idx}`} />;
+        return <img className="flex w-full h-full rounded object-cover" src={src} alt={`post-img`} />;
     }
 
     return (
